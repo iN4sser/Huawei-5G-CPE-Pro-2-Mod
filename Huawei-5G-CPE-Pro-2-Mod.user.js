6426fc7929978864a1ce5a24e83e63e31d32db11
iN4sser <iN4sser@users.noreply.github.com>
2021-02-17 11:40:10 -0800

    Update Huawei-5G-CPE-Pro-2-Mod.user.js

--- Huawei-5G-CPE-Pro-2-Mod.user.js
+++ Huawei-5G-CPE-Pro-2-Mod.user.js
@@ -2,8 +2,8 @@
 // @name        Huawei 5G CPE Pro 2 Mod
 // @icon        https://i.imgur.com/OsLkmXp.png
 // @license     GPL version 2 or any later version; http://www.gnu.org/licenses/gpl-2.0.txt
-// @updateURL   https://github.com/iN4sser/Huawei-5G-CPE-Pro-2-Mod/raw/master/Huawei-5G-CPE-Pro-2-Mod.user.js
-// @downloadURL https://github.com/iN4sser/Huawei-5G-CPE-Pro-2-Mod/raw/master/Huawei-5G-CPE-Pro-2-Mod.user.js
+// @updateURL   https://github.com/iN4sser/Huawei-5G-CPE-Pro-2-Mod/raw/main/Huawei-5G-CPE-Pro-2-Mod.user.js
+// @downloadURL https://github.com/iN4sser/Huawei-5G-CPE-Pro-2-Mod/raw/main/Huawei-5G-CPE-Pro-2-Mod.user.js
 // @include     *://192.168.*.1/*
 // @version     1.1
 // @author      iN4sser
